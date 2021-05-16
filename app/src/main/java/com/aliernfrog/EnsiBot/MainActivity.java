846c085eea538ce85787b9eb3df5925fe6234838
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2021-05-17 00:42:55 +0300

    Initial commit

---
+++ app/src/main/java/com/aliernfrog/EnsiBot/MainActivity.java
@@ -0,0 +1,165 @@
+package com.aliernfrog.EnsiBot;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.os.Handler;
+import android.text.Html;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import com.aliernfrog.EnsiBot.utils.EnsiUtil;
+import com.aliernfrog.EnsiBot.utils.FileUtil;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
+@SuppressLint("CommitPrefEdits")
+public class MainActivity extends AppCompatActivity {
+    ImageView options;
+    ScrollView chatScroll;
+    LinearLayout chatRoot;
+    EditText chatInput;
+    ImageView chatSend;
+
+    Drawable ensiAvatar;
+    Drawable userAvatar;
+
+    String ensiUsername = "<font color=yellow>Ensi</font>";
+    String userUsername = "Some frok";
+
+    SharedPreferences config;
+
+    ArrayList<String> savedWords;
+    ArrayList<String> savedVerbs;
+
+    Boolean[] saveChances = {false,false,true};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ensiAvatar = ContextCompat.getDrawable(getApplicationContext(), R.drawable.ensi);
+        userAvatar = ContextCompat.getDrawable(getApplicationContext(), R.drawable.user);
+
+        options = findViewById(R.id.main_options);
+        chatScroll = findViewById(R.id.main_chatScroll);
+        chatRoot = findViewById(R.id.main_chatRoot);
+        chatInput = findViewById(R.id.main_chatInput);
+        chatSend = findViewById(R.id.main_chatSend);
+
+        config = getSharedPreferences("APP_CONFIG", MODE_PRIVATE);
+
+        sendMessage(ensiAvatar, ensiUsername, "hi bro");
+
+        getSavedWordsAndVerbs();
+        setListeners();
+    }
+
+    void sendMessage(@Nullable Drawable avatar, String username, String content) {
+        if (content.replaceAll(" ", "").equals("")) return;
+        ViewGroup message = (ViewGroup) getLayoutInflater().inflate(R.layout.message, chatRoot, false);
+        LinearLayout messageView = message.findViewById(R.id.message_linear);
+        ImageView avatarView = message.findViewById(R.id.message_author_avatar);
+        TextView usernameView = message.findViewById(R.id.message_author_username);
+        TextView contentView = message.findViewById(R.id.message_content);
+        if (avatar != null) avatarView.setImageDrawable(avatar);
+        usernameView.setText(Html.fromHtml(username));
+        usernameView.setOnClickListener(v -> mention(usernameView.getText().toString()));
+        contentView.setText(Html.fromHtml(content));
+        messageView.setOnLongClickListener(v -> {
+            deleteMessage(message);
+            return true;
+        });
+
+        chatRoot.addView(message);
+        scrollToBottom();
+        onMessage(username, content);
+    }
+
+    void deleteMessage(ViewGroup message) {
+        chatRoot.removeView(message);
+    }
+
+    void sendStory(String username, String content) {
+        sendMessage(ensiAvatar, ensiUsername, EnsiUtil.buildMessage(username, content, savedWords, savedVerbs));
+        Random random = new Random();
+        Boolean doSave = saveChances[random.nextInt(saveChances.length)];
+        if (doSave) {
+            String[] args = content.split(" ");
+            String word = args[random.nextInt(args.length)];
+            saveWord(word);
+        }
+    }
+
+    void mention(String username) {
+        String currentText = chatInput.getText().toString();
+        String mention = EnsiUtil.buildMention(username);
+        String finalText = currentText+mention;
+        chatInput.setText(finalText);
+    }
+
+    void scrollToBottom() {
+        Handler handler = new Handler();
+        handler.postDelayed(() -> chatScroll.fullScroll(View.FOCUS_DOWN), 100);
+    }
+
+    void onMessage(String username, String content) {
+        if (username.equals(ensiUsername) || username.equals("")) return;
+        Handler handler = new Handler();
+        handler.postDelayed(() -> sendStory(username, content), 150);
+    }
+
+    void getSavedWordsAndVerbs() {
+        String words = config.getString("savedWords", "");
+        String verbs = config.getString("savedVerbs", "");
+        savedWords = new ArrayList<>(Arrays.asList(words.split("\n")));
+        savedVerbs = new ArrayList<>(Arrays.asList(verbs.split("\n")));
+    }
+
+    void saveWord(String word) {
+        word = word.toLowerCase();
+        try {
+            if (savedWords.contains(word) || savedVerbs.contains(word)) return;
+            String folder = getExternalFilesDir("saved").toString();
+            boolean isVerb = word.endsWith("ed");
+            if (!isVerb) {
+                savedWords.add(word);
+                FileUtil.saveFile(folder, "words.txt", EnsiUtil.listJoin("\n", savedWords));
+            } else {
+                savedVerbs.add(word);
+                FileUtil.saveFile(folder, "verbs.txt", EnsiUtil.listJoin("\n", savedVerbs));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    void switchActivity(Class activity) {
+        Intent intent = new Intent(this, activity);
+        startActivity(intent);
+    }
+
+    void setListeners() {
+        options.setOnClickListener(v -> switchActivity(DlcActivity.class));
+
+        chatSend.setOnClickListener(v -> {
+            sendMessage(userAvatar, userUsername, chatInput.getText().toString());
+            chatInput.setText("");
+        });
+    }
+}
---
+++ app/src/main/java/com/aliernfrog/EnsiBot/SplashActivity.java
@@ -0,0 +1,82 @@
+package com.aliernfrog.EnsiBot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.aliernfrog.EnsiBot.utils.FileUtil;
+
+import java.io.File;
+
+@SuppressLint("CommitPrefEdits")
+public class SplashActivity extends AppCompatActivity {
+    String dataPath;
+    String wordsPath;
+    String verbsPath;
+
+    String wordsFileName = "words.txt";
+    String verbsFileName = "verbs.txt";
+
+    String defaultWords = "me\nyou\nwe\nthey\nalierns\nindinibee\nbees\nmomes\nfrogs\nmouse\nchicken\nfurries\nfrog\nExi's basement\nfree candies\nensi\nvan\nlaptop\nmarchmilos\nmouse\nuwu\nhackers\n\uD83D\uDE10\ntryhards\nmohamme\n\uD83C\uDDE7\uD83C\uDDF7\nat 3am\nat 3am *gone wrong*\ndaily\nopa\nfrok\nalizade\ndislike\nreply\ninfini\nmohamet\nsuicide\n ";
+    String defaultVerbs = "went to\nate\ndrove\nkidnapped\ntalked\nhacked\ngermanized\nmoused\nstalked\ntrapped\nraped\nexported\nadded\nsnorted\nliked\nred\nmohammed\n ";
+
+    SharedPreferences config;
+    SharedPreferences.Editor configEdit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        dataPath = getExternalFilesDir("saved").toString();
+        wordsPath = dataPath+"/"+wordsFileName;
+        verbsPath = dataPath+"/"+verbsFileName;
+
+        config = getSharedPreferences("APP_CONFIG", MODE_PRIVATE);
+        configEdit = config.edit();
+
+        checkFiles();
+    }
+
+    void checkFiles() {
+        File wordsFile = new File(wordsPath);
+        File verbsFile = new File(verbsPath);
+        if (!wordsFile.exists()) saveFile(wordsFileName, defaultWords);
+        if (!verbsFile.exists()) saveFile(verbsFileName, defaultVerbs);
+        getSavedWordsAndVerbs();
+    }
+
+    void getSavedWordsAndVerbs() {
+        try {
+            String savedWords = FileUtil.readFile(wordsPath);
+            String savedVerbs = FileUtil.readFile(verbsPath);
+            configEdit.putString("savedWords", savedWords);
+            configEdit.putString("savedVerbs", savedVerbs);
+            configEdit.commit();
+            switchActivity();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    void saveFile(String name, String content) {
+        try {
+            FileUtil.saveFile(dataPath, name, content);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    void switchActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        Handler handler = new Handler();
+        handler.postDelayed(() -> {
+            startActivity(intent);
+            finish();
+        }, 500);
+    }
+}
