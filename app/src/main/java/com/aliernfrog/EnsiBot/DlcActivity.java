9d4fa375759aa96249c8a1ee6a7fa7bf7609a206
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2021-06-16 20:36:39 +0300

    Apply words packs

--- app/src/main/java/com/aliernfrog/EnsiBot/DlcActivity.java
+++ app/src/main/java/com/aliernfrog/EnsiBot/DlcActivity.java
@@ -5,12 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.StrictMode;
+import android.text.Html;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.aliernfrog.EnsiBot.utils.FileUtil;
 import com.aliernfrog.EnsiBot.utils.WebUtil;
 
 import org.json.JSONArray;
@@ -20,9 +23,11 @@ public class DlcActivity extends AppCompatActivity {
     LinearLayout root;
     ProgressBar loading;
 
+    String dataPath;
+    String wordsFileName = "words.txt";
+    String verbsFileName = "verbs.txt";
+    String url = "https://ensiapp.aliernfrog.repl.co";
     JSONArray rawDlcs;
-
-    String url = "https://ensibot-discord.aliernfrog.repl.co";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,8 +39,16 @@ public class DlcActivity extends AppCompatActivity {
         root = findViewById(R.id.dlc_root);
         loading = findViewById(R.id.dlc_loading);
 
+        dataPath = getExternalFilesDir("saved").toString();
+
         Handler handler = new Handler();
         handler.postDelayed(this::getDlcs, 1000);
+    }
+
+    void applyDlc(String words, String verbs) {
+        saveFile(wordsFileName, words);
+        saveFile(verbsFileName, verbs);
+        Toast.makeText(getApplicationContext(), R.string.dlc_applied, Toast.LENGTH_SHORT).show();
     }
 
     void getDlcs() {
@@ -65,19 +78,29 @@ public class DlcActivity extends AppCompatActivity {
 
     void setDlcView(JSONObject object, ViewGroup dlc) {
         try {
+            LinearLayout dlcLinear = dlc.findViewById(R.id.dlc_linear);
             TextView nameView = dlc.findViewById(R.id.dlc_name);
+            TextView descView = dlc.findViewById(R.id.dlc_desc);
             TextView wordsView = dlc.findViewById(R.id.dlc_words);
             TextView verbsView = dlc.findViewById(R.id.dlc_verbs);
-            TextView idView = dlc.findViewById(R.id.dlc_id);
             String name = object.getString("name");
+            String desc = object.getString("description");
             String words = object.getString("words");
             String verbs = object.getString("verbs");
-            String id = object.getString("_id");
             nameView.setText(name);
+            descView.setText(Html.fromHtml(desc));
             wordsView.setText(words);
             verbsView.setText(verbs);
-            idView.setText(id);
+            dlcLinear.setOnClickListener(v -> applyDlc(words, verbs));
             root.addView(dlc);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    void saveFile(String name, String content) {
+        try {
+            FileUtil.saveFile(dataPath, name, content);
         } catch (Exception e) {
             e.printStackTrace();
         }
