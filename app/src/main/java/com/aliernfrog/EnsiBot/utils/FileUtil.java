6cc9a9cb0e2e5d2114e54bb8295c86bef26f15ce
aliernfrog <blogsimulasyon@gmail.com>
2021-06-17 21:13:35 +0300

    Avatar & username customization

--- app/src/main/java/com/aliernfrog/EnsiBot/utils/FileUtil.java
+++ app/src/main/java/com/aliernfrog/EnsiBot/utils/FileUtil.java
@@ -2,10 +2,36 @@ package com.aliernfrog.EnsiBot.utils;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class FileUtil {
+    public static void copyFile(String source, String destination) throws Exception {
+        File src = new File(source);
+        InputStream in;
+        OutputStream out;
+        if (src.isFile()) {
+            in =  new FileInputStream(new File(source));
+            out = new FileOutputStream(new File(destination));
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = in.read(buffer)) > 0) {
+                out.write(buffer, 0, length);
+            }
+            in.close();
+            out.close();
+        } else {
+            File[] files = src.listFiles();
+            for (int i = 0; i < files.length; i++) {
+                copyFile(files[i].getPath(), destination+"/"+files[i].getName());
+            }
+        }
+    }
+
     public static String readFile(String source) throws Exception {
         BufferedReader reader = new BufferedReader(new FileReader(new File(source).getPath()));
         String _line;
