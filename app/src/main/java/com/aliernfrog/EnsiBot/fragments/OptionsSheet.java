69ab2a94fe6a266a5f9450bc9384d3e2d3739175
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2021-10-29 23:14:15 +0300

    Add OptionsSheet

---
+++ app/src/main/java/com/aliernfrog/EnsiBot/fragments/OptionsSheet.java
@@ -0,0 +1,85 @@
+package com.aliernfrog.EnsiBot.fragments;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.aliernfrog.EnsiBot.ChatActivity;
+import com.aliernfrog.EnsiBot.OptionsActivity;
+import com.aliernfrog.EnsiBot.R;
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+import java.io.File;
+
+public class OptionsSheet extends BottomSheetDialogFragment {
+    LinearLayout avatar_linear;
+    ImageView avatar;
+    TextView username;
+    Button starboard;
+    Button options;
+
+    String avatarPath;
+
+    SharedPreferences config;
+
+    Context context;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.sheet_options, container, false);
+
+        avatar_linear = view.findViewById(R.id.optionssheet_avatar);
+        avatar = view.findViewById(R.id.optionssheet_avatar_avatar);
+        username = view.findViewById(R.id.optionssheet_avatar_username);
+        starboard = view.findViewById(R.id.optionssheet_starboard);
+        options = view.findViewById(R.id.optionssheet_options);
+
+        context = getActivity();
+
+        if (context != null) avatarPath = context.getExternalFilesDir("saved").toString()+"/avatar.png";
+        if (context != null) config = context.getSharedPreferences("APP_CONFIG", Context.MODE_PRIVATE);
+
+        getUsername();
+        getAvatar();
+        setListeners();
+
+        return view;
+    }
+
+    void getUsername() {
+        String _username = config.getString("username", "Some frok");
+        username.setText(_username);
+    }
+
+    void getAvatar() {
+        File avatarFile = new File(avatarPath);
+        if (avatarFile.exists()) {
+            Drawable userAvatar = Drawable.createFromPath(avatarPath);
+            avatar.setImageDrawable(userAvatar);
+        }
+    }
+
+    void switchActivity(Class activity, Boolean dismiss) {
+        Intent intent = new Intent(context, activity);
+        startActivity(intent);
+        if (dismiss) dismiss();
+    }
+
+    void setListeners() {
+        avatar_linear.setOnClickListener(view -> switchActivity(OptionsActivity.class, true));
+        options.setOnClickListener(view -> switchActivity(OptionsActivity.class, true));
+    }
+}
