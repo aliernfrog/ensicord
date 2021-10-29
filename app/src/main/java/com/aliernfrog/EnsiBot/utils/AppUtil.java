68f394a5241a8cec5a51f7303bcaac328823663f
aliernfrog <blogsimulasyon@gmail.com>
2021-10-29 23:15:37 +0300

    Add AppUtil

---
+++ app/src/main/java/com/aliernfrog/EnsiBot/utils/AppUtil.java
@@ -0,0 +1,48 @@
+package com.aliernfrog.EnsiBot.utils;
+
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.annotation.SuppressLint;
+import android.view.MotionEvent;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class AppUtil {
+    public static void handleOnPressEvent(View view, MotionEvent event, @Nullable Runnable onClick) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                ObjectAnimator scaleDownX = ObjectAnimator.ofFloat(view, "scaleX", 0.9f);
+                ObjectAnimator scaleDownY = ObjectAnimator.ofFloat(view, "scaleY", 0.9f);
+                scaleDownX.setDuration(100);
+                scaleDownY.setDuration(100);
+                AnimatorSet scaleDown = new AnimatorSet();
+                scaleDown.play(scaleDownX).with(scaleDownY);
+                scaleDown.start();
+                break;
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_CANCEL:
+                if (onClick != null && event.getAction() == MotionEvent.ACTION_UP) onClick.run();
+                ObjectAnimator scaleUpX = ObjectAnimator.ofFloat(view, "scaleX", 1f);
+                ObjectAnimator scaleUpY = ObjectAnimator.ofFloat(view, "scaleY", 1f);
+                scaleUpX.setDuration(100);
+                scaleUpY.setDuration(100);
+                AnimatorSet scaleUp = new AnimatorSet();
+                scaleUp.play(scaleUpX).with(scaleUpY);
+                scaleUp.start();
+                break;
+        }
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    public static void handleOnPressEvent(View view, @Nullable Runnable onClick) {
+        view.setOnTouchListener((v, event) -> {
+            handleOnPressEvent(v, event, onClick);
+            return true;
+        });
+    }
+
+    public static void handleOnPressEvent(View view) {
+        handleOnPressEvent(view, (Runnable) null);
+    }
+}
