dd6f13415b1d17277b28fb40f8628e7a2c2c7378
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2021-10-30 18:57:57 +0300

    Only finish all activities if applied a DLC

--- app/src/main/java/com/aliernfrog/EnsiBot/OptionsActivity.java
+++ app/src/main/java/com/aliernfrog/EnsiBot/OptionsActivity.java
@@ -154,7 +154,6 @@ public class OptionsActivity extends AppCompatActivity implements PickiTCallback
     public void onBackPressed() {
         pickiT.deleteTemporaryFile(this);
         finish();
-        switchActivity(SplashActivity.class);
         super.onBackPressed();
     }
 }
