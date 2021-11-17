c2dce641795dd4b9c8da8085a3c5cf200847c676
aliernfrog <blogsimulasyon@gmail.com>
2021-11-17 21:39:24 +0300

    Start ChatActivity on startup

--- app/src/main/java/com/aliernfrog/EnsiBot/SplashActivity.java
+++ app/src/main/java/com/aliernfrog/EnsiBot/SplashActivity.java
@@ -4,12 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-
-import com.aliernfrog.EnsiBot.utils.FileUtil;
-
-import java.io.File;
 
 @SuppressLint("CommitPrefEdits")
 public class SplashActivity extends AppCompatActivity {
