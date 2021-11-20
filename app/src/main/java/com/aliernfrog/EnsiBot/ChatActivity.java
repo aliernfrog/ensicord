01d75857d92d28206639dd39fd353a690929affb
aliernfrog <blogsimulasyon@gmail.com>
2021-11-20 18:52:26 +0300

    Message context menu

--- app/src/main/java/com/aliernfrog/EnsiBot/ChatActivity.java
+++ app/src/main/java/com/aliernfrog/EnsiBot/ChatActivity.java
@@ -12,7 +12,6 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
-import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.Toast;
 
