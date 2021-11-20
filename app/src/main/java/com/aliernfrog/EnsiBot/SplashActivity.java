060488cc92b584511c75702e8a714a0c4af08f68
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2021-11-20 23:48:54 +0300

    Add autoUpdate option

--- app/src/main/java/com/aliernfrog/EnsiBot/SplashActivity.java
+++ app/src/main/java/com/aliernfrog/EnsiBot/SplashActivity.java
@@ -7,7 +7,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.StrictMode;
-import android.widget.Toast;
 
 import com.aliernfrog.EnsiBot.utils.AppUtil;
 import com.aliernfrog.EnsiBot.utils.FileUtil;
