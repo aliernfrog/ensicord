02c1ea625a7040315249ac38e2fc13539ea457ad
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2022-07-03 21:43:28 +0300

    Remove "()"

--- app/src/main/java/com/aliernfrog/ensicord/MainActivity.kt
+++ app/src/main/java/com/aliernfrog/ensicord/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            EnsicordTheme() {
+            EnsicordTheme {
                 Box(Modifier.background(MaterialTheme.colors.background).fillMaxSize())
                 SystemBars()
                 Navigation()
