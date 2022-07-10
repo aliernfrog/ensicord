3c110916952b63ad18da9611a3a6617e755aecb3
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2022-07-10 23:48:05 +0300

    Highlight mentions

--- app/src/main/java/com/aliernfrog/ensicord/ui/composable/EnsicordMessage.kt
+++ app/src/main/java/com/aliernfrog/ensicord/ui/composable/EnsicordMessage.kt
@@ -1,6 +1,7 @@
 package com.aliernfrog.ensicord.ui.composable
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
@@ -11,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -18,9 +20,11 @@ import com.aliernfrog.ensicord.data.Message
 import com.aliernfrog.ensicord.utils.GeneralUtil
 
 @Composable
-fun EnsicordMessage(message: Message, onNameClick: (() -> Unit)? = null) {
+fun EnsicordMessage(message: Message, checkMention: String? = null, onNameClick: (() -> Unit)? = null) {
     val avatar = GeneralUtil.getAvatarId(message.author.avatar)
-    Row(Modifier.clickable{}.padding(8.dp)) {
+    var modifier = Modifier.clickable{}
+    if (checkMention != null && message.content.contains("@$checkMention")) modifier = modifier.background(Color(0x2BFFF700))
+    Row(modifier.padding(8.dp)) {
         Image(painter = painterResource(id = avatar), contentDescription = "", Modifier.padding(end = 8.dp).clip(CircleShape).size(44.dp, 44.dp))
         Column(Modifier.weight(1f)) {
             Text(
