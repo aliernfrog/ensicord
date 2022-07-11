8acfdd7ee0ef3fd96e512476e22198bc46a377cf
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2022-07-11 12:19:55 +0300

    UserStatus data class

--- app/src/main/java/com/aliernfrog/ensicord/ui/screen/ChatScreen.kt
+++ app/src/main/java/com/aliernfrog/ensicord/ui/screen/ChatScreen.kt
@@ -1,7 +1,9 @@
 package com.aliernfrog.ensicord.ui.screen
 
+import android.content.Context
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.ViewModel
 import androidx.navigation.NavController
 import com.aliernfrog.ensicord.data.User
@@ -12,10 +14,10 @@ import com.aliernfrog.ensicord.utils.EnsiUtil
 import com.xinto.overlappingpanels.OverlappingPanels
 import com.xinto.overlappingpanels.rememberOverlappingPanelsState
 
-class ChatModel: ViewModel() {
+class ChatModel(context: Context): ViewModel() {
     val chosenChannel = "#ensicord-development"
     val userUser = User("user", "Some frok", "user")
-    val ensiUser = User("ensi", "Ensi", "ensi", EnsiUtil.getResponse(), true)
+    val ensiUser = User("ensi", "Ensi", "ensi", EnsiUtil.getStatus(context), true)
     val users = listOf(userUser,ensiUser)
     val channels = listOf("#ensicord-development")
 }
@@ -23,7 +25,7 @@ class ChatModel: ViewModel() {
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun ChatScreen(navController: NavController) {
-    val chatModel = ChatModel()
+    val chatModel = ChatModel(LocalContext.current)
     val panelsState = rememberOverlappingPanelsState()
     OverlappingPanels(
         panelsState = panelsState,
