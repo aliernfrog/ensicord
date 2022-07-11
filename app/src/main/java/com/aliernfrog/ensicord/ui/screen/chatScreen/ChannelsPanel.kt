f34d798c13f476762b7178440d5d6379b33632a4
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2022-07-11 10:43:34 +0300

    Add options button

--- app/src/main/java/com/aliernfrog/ensicord/ui/screen/chatScreen/ChannelsPanel.kt
+++ app/src/main/java/com/aliernfrog/ensicord/ui/screen/chatScreen/ChannelsPanel.kt
@@ -8,16 +8,22 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.aliernfrog.ensicord.R
 import com.aliernfrog.ensicord.ui.composable.EnsicordChannel
+import com.aliernfrog.ensicord.ui.composable.EnsicordBorderlessButton
 import com.aliernfrog.ensicord.ui.composable.EnsicordUser
 import com.aliernfrog.ensicord.ui.screen.ChatModel
 
 @Composable
 fun channelsPanel(chatModel: ChatModel): @Composable (BoxScope.() -> Unit) {
+    val context = LocalContext.current
     return {
         Column(Modifier.clip(RoundedCornerShape(topEnd = 20.dp)).fillMaxSize().background(MaterialTheme.colors.secondary)) {
             LazyColumn(Modifier.weight(1f)) {
@@ -27,7 +33,11 @@ fun channelsPanel(chatModel: ChatModel): @Composable (BoxScope.() -> Unit) {
             }
             Column(Modifier.background(MaterialTheme.colors.secondary).clickable{}) {
                 Box(modifier = Modifier.alpha(0.2f).fillMaxWidth().height(1.dp).background(MaterialTheme.colors.onBackground))
-                EnsicordUser(user = chatModel.userUser)
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    EnsicordUser(user = chatModel.userUser, Modifier.weight(1f))
+                    EnsicordBorderlessButton(painterLight = painterResource(id = R.drawable.gear_black), painterDark = painterResource(id = R.drawable.gear_white), contentDescription = context.getString(R.string.options)) {}
+                    Spacer(Modifier.width(8.dp))
+                }
             }
         }
     }
