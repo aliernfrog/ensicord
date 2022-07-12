ba1375de1af01ec0c1efb6a4eee0e8d0958893d4
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2022-07-12 21:52:03 +0300

    #general

--- app/src/main/java/com/aliernfrog/ensicord/model/ChatModel.kt
+++ app/src/main/java/com/aliernfrog/ensicord/model/ChatModel.kt
@@ -22,10 +22,11 @@ class ChatModel(context: Context, config: SharedPreferences): ViewModel() {
     val ensiUser = User("ensi", "Ensi", "ensi", EnsiUtil.getStatus(context), true)
     var users = listOf(userUser,ensiUser)
 
-    private val generalChannel = Channel("#ensicord-development")
+    private val generalChannel = Channel("#general")
+    private val devChannel = Channel("#ensicord-development")
     private val starboardChannel = Channel("#starboard", readOnly = true)
     var chosenChannel by mutableStateOf(generalChannel)
-    val channels = listOf(generalChannel,starboardChannel)
+    val channels = listOf(generalChannel,devChannel,starboardChannel)
 
     fun updateUser(newUserName: String? = null, newUserStatus: String? = null) {
         userUser = User(
