d83c9b7ecbb256f7d8ee4070a5c5a967335c75a1
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2022-07-17 21:26:03 +0300

    feat: progress indicator for addons

--- app/src/main/java/com/aliernfrog/ensicord/ui/screen/AddonsScreen.kt
+++ app/src/main/java/com/aliernfrog/ensicord/ui/screen/AddonsScreen.kt
@@ -1,7 +1,12 @@
 package com.aliernfrog.ensicord.ui.screen
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.aliernfrog.ensicord.R
 import com.aliernfrog.ensicord.model.AddonsModel
@@ -12,8 +17,12 @@ import com.aliernfrog.ensicord.ui.composable.EnsicordBaseScaffold
 fun AddonsScreen(navController: NavController, addonsModel: AddonsModel) {
     val context = LocalContext.current
     EnsicordBaseScaffold(title = context.getString(R.string.addons), navController = navController) {
-        addonsModel.addons.forEach { addon ->
-            EnsicordAddon(addon)
+        if (addonsModel.loaded) {
+            addonsModel.addons.forEach { addon ->
+                EnsicordAddon(addon)
+            }
+        } else {
+            CircularProgressIndicator(Modifier.align(CenterHorizontally).padding(top = 10.dp))
         }
     }
 }
