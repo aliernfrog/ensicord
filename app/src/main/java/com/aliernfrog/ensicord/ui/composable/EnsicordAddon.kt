81abace3d97a9d5ae1714b92de2ecdd79a1f7e32
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2022-07-19 22:34:47 +0300

    feat: adjust addons padding & title size

--- app/src/main/java/com/aliernfrog/ensicord/ui/composable/EnsicordAddon.kt
+++ app/src/main/java/com/aliernfrog/ensicord/ui/composable/EnsicordAddon.kt
@@ -1,15 +1,18 @@
 package com.aliernfrog.ensicord.ui.composable
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.aliernfrog.ensicord.data.Addon
 
 @Composable
 fun EnsicordAddon(addon: Addon, onClick: () -> Unit) {
     EnsicordColumnRounded(onClick = onClick) {
-        Text(addon.name, fontWeight = FontWeight.Bold, fontSize = 30.sp)
-        Text(addon.description)
+        Text(addon.name, fontWeight = FontWeight.Bold, fontSize = 25.sp, modifier = Modifier.padding(horizontal = 8.dp))
+        Text(addon.description, modifier = Modifier.padding(horizontal = 8.dp))
     }
 }
