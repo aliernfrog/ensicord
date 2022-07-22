98bcce14910db4fc68b86944cf784d502a8bd45b
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2022-07-22 22:15:36 +0300

    refactor: adjust constants order

--- app/src/main/java/com/aliernfrog/ensicord/Constant.kt
+++ app/src/main/java/com/aliernfrog/ensicord/Constant.kt
@@ -1,13 +1,13 @@
 package com.aliernfrog.ensicord
 
-object Theme {
-    const val SYSTEM = 0
-    const val LIGHT = 1
-    const val DARK = 2
-}
-
 object AddonFetchingState {
     const val ADDONS_LOADING = 0
     const val ADDONS_DONE = 1
     const val ADDONS_ERROR = 2
+}
+
+object Theme {
+    const val SYSTEM = 0
+    const val LIGHT = 1
+    const val DARK = 2
 }
