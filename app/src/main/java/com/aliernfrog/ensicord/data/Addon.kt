5b097ddcd9b182227f60e23cd7ae442bcac1c4d9
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2022-07-24 14:10:18 +0300

    feat: handle more addon errors

--- app/src/main/java/com/aliernfrog/ensicord/data/Addon.kt
+++ app/src/main/java/com/aliernfrog/ensicord/data/Addon.kt
@@ -3,6 +3,7 @@ package com.aliernfrog.ensicord.data
 data class Addon(
     val name: String,
     val description: String,
+    val error: Boolean = false,
     val setAppTheme: Int? = null,
     val setEnsiUserName: String? = null
 )
