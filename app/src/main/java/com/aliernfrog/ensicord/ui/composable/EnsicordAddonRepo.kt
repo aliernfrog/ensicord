4e311eaeb62cb9a44be23fb949a200065fbe2ab5
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2022-07-31 14:33:24 +0300

    feat: adjust addon repo composable

--- app/src/main/java/com/aliernfrog/ensicord/ui/composable/EnsicordAddonRepo.kt
+++ app/src/main/java/com/aliernfrog/ensicord/ui/composable/EnsicordAddonRepo.kt
@@ -1,15 +1,19 @@
 package com.aliernfrog.ensicord.ui.composable
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import com.aliernfrog.ensicord.R
 
 @Composable
 fun EnsicordAddonRepo(url: String, onRemove: () -> Unit) {
     val context = LocalContext.current
     EnsicordColumnRounded {
-        Text(text = url)
-        EnsicordButton(title = context.getString(R.string.action_remove), onClick = onRemove)
+        Text(text = url, modifier = Modifier.padding(horizontal = 8.dp))
+        EnsicordButton(title = context.getString(R.string.action_remove), backgroundColor = MaterialTheme.colors.error, contentColor = MaterialTheme.colors.onError, onClick = onRemove)
     }
 }
