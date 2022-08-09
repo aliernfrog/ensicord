e8869632e06c62e52af6f537a6135b77186177ef
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2022-08-09 23:39:22 +0300

    feat: bottom space for UserSheet

--- app/src/main/java/com/aliernfrog/ensicord/ui/sheet/UserSheet.kt
+++ app/src/main/java/com/aliernfrog/ensicord/ui/sheet/UserSheet.kt
@@ -2,9 +2,7 @@ package com.aliernfrog.ensicord.ui.sheet
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.MaterialTheme
@@ -30,6 +28,7 @@ fun UserSheet(user: User? = null, sheetState: ModalBottomSheetState, onNameClick
             Image(painter = GeneralUtil.getAvatarPainter(user.avatar), contentDescription = user.name, modifier = Modifier.padding(vertical = 16.dp).size(120.dp).clip(CircleShape).align(Alignment.Start))
             Text(user.name, color = MaterialTheme.colors.onBackground, fontWeight = FontWeight.Bold, fontSize = 30.sp, modifier = Modifier.fillMaxWidth().clickable { if (onNameClick != null) onNameClick() })
             if (user.status != null && (!user.status.type.isNullOrEmpty() || !user.status.name.isNullOrEmpty())) Text(text = GeneralUtil.getUserStatusText(user.status), color = MaterialTheme.colors.onBackground, fontSize = 14.sp, modifier = Modifier.alpha(0.7f))
+            Spacer(Modifier.height(100.dp))
         }
     }
 }
