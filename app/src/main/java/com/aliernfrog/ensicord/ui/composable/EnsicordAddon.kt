f110a3e5957681ae7285d1a0cf0865e767517dfd
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2022-11-06 18:18:51 +0300

    feat: use Button in EnsicordAddon

--- app/src/main/java/com/aliernfrog/ensicord/ui/composable/EnsicordAddon.kt
+++ app/src/main/java/com/aliernfrog/ensicord/ui/composable/EnsicordAddon.kt
@@ -1,6 +1,9 @@
 package com.aliernfrog.ensicord.ui.composable
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
@@ -17,37 +20,45 @@ import com.aliernfrog.ensicord.data.Addon
 
 @Composable
 fun EnsicordAddon(addon: Addon, onClick: () -> Unit) {
-    EnsicordColumnRounded(
-        onClick = if (!addon.error) onClick else null,
-        color = if (!addon.error) MaterialTheme.colors.secondary else MaterialTheme.colors.error
+    var showThumbnail by remember { mutableStateOf(true) }
+    Button(
+        onClick = onClick,
+        modifier = Modifier.padding(8.dp).fillMaxWidth(),
+        shape = RoundedCornerShape(20.dp),
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = if (addon.error) MaterialTheme.colors.error else MaterialTheme.colors.secondary,
+            contentColor = if (addon.error) MaterialTheme.colors.onError else MaterialTheme.colors.onSecondary
+        ),
+        contentPadding = PaddingValues(8.dp)
     ) {
-        var showThumbnail by remember { mutableStateOf(true) }
-        Row(Modifier.padding(horizontal = 8.dp), verticalAlignment = Alignment.CenterVertically) {
-            if (!addon.thumbnailUrl.isNullOrBlank() && showThumbnail) {
-                AsyncImage(
-                    model = addon.thumbnailUrl,
-                    contentDescription = null,
-                    modifier = Modifier.padding(end = 8.dp).size(45.dp),
-                    onError = { showThumbnail = false }
+        Column(modifier = Modifier.fillMaxWidth()) {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                if (!addon.thumbnailUrl.isNullOrBlank() && showThumbnail) {
+                    AsyncImage(
+                        model = addon.thumbnailUrl,
+                        contentDescription = null,
+                        modifier = Modifier.padding(end = 8.dp).size(45.dp),
+                        onError = { showThumbnail = false }
+                    )
+                }
+                Text(
+                    text = if (!addon.error) addon.name else LocalContext.current.getString(R.string.warning_somethingWentWrong),
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 22.sp
                 )
             }
+            Spacer(Modifier.height(2.dp))
             Text(
-                text = if (!addon.error) addon.name else LocalContext.current.getString(R.string.warning_somethingWentWrong),
-                fontWeight = FontWeight.Bold, fontSize = 22.sp,
-                color = if (!addon.error) MaterialTheme.colors.onSecondary else MaterialTheme.colors.onError
+                text = addon.description,
+                color = if (!addon.error) MaterialTheme.colors.onSecondary else MaterialTheme.colors.onError,
+                modifier = Modifier.alpha(0.8f)
+            )
+            Text(
+                text = addon.repo,
+                fontSize = 10.sp,
+                color = if (!addon.error) MaterialTheme.colors.onSecondary else MaterialTheme.colors.onError,
+                modifier = Modifier.alpha(0.5f)
             )
         }
-        Spacer(Modifier.height(2.dp))
-        Text(
-            text = addon.description,
-            color = if (!addon.error) MaterialTheme.colors.onSecondary else MaterialTheme.colors.onError,
-            modifier = Modifier.padding(horizontal = 8.dp).alpha(0.8f)
-        )
-        Text(
-            text = addon.repo,
-            fontSize = 10.sp,
-            color = if (!addon.error) MaterialTheme.colors.onSecondary else MaterialTheme.colors.onError,
-            modifier = Modifier.padding(horizontal = 8.dp).alpha(0.5f)
-        )
     }
 }
